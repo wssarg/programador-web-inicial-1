c1b6d882560bc460510480d966e95d83c53e9f3b
wssarg <wssarg@gmail.com>
2023-01-15 22:02:51 -0300

    cambios por tp8

--- unidad7.js
+++ unidad7.js
@@ -1,5 +1,11 @@
 
 var nombre = prompt('Hola buen dia, podría indicarme su nombre?');
+
+// para el tp8
+while (nombre.length == 0) {
+    var nombre = prompt('Hola buen dia, podría indicarme su nombre?');
+}
+
 alert('Hola '+nombre+', antes de entrar, le invitamos a contestar una breve encuenta de 4 preguntas');
 var edad = prompt(nombre+', podrias decirme tu edad?');
 var tarjeta = prompt(nombre+', podrias indicarme si operas con tarjeta de credito?');
@@ -8,3 +14,21 @@ var first = new Date('12/25/2023');
 var second = new Date();
 const dias_para_navidad = Math.floor((first - second) / (1000 * 60 * 60 * 24));
 alert('Faltan '+dias_para_navidad+' días para la proxima navidad.');
+
+// para el tp8
+tarjeta = tarjeta.toUpperCase();
+if (tarjeta == '1' || tarjeta == 'SI') {
+    tarjeta = 'SI';
+}
+else{
+    tarjeta = 'NO';
+}
+
+habitual = habitual.toUpperCase();
+if (habitual == '1' || habitual == 'SI') {
+    habitual = 'SI';
+}
+else{
+    habitual = 'NO';
+}
+
